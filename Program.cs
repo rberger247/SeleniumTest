853dcb6322497195bd59aace81e552f442d8f98b
Rafael Berger <Rafiberger613@gmail.com>
2017-11-23 10:36:56 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("test-type");
+
+            options.AddArguments("chrome.switches", "--disable-extensions --disable-extensions-file-access-check --disable-extensions-http-throttling");
+            IWebDriver driver = new ChromeDriver(@"C:\Users\Rafib\Downloads\chromedriver_win32/");
+      
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            // Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+           
+            //Preform operation
+            element.SendKeys("ExecuteAutomation");
+            //driver.GetScreenshot().SaveAsFile(@"F:\temp\screen.png", ImageFormat.Png);
+        }
+    }
+}
